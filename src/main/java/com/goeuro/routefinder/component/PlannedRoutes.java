693dfbca42f69a0d46816e4a6cf42888ae369f40
arpitmehta-it <arpitmehta_it@yahoo.com>
2016-12-28 19:35:09 +0100

    Update PlannedRoutes.java

--- src/main/java/com/goeuro/routefinder/component/PlannedRoutes.java
+++ src/main/java/com/goeuro/routefinder/component/PlannedRoutes.java
@@ -1,5 +1,7 @@
 package com.goeuro.routefinder.component;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -14,46 +16,51 @@ import java.util.Scanner;
 @Component
 public class PlannedRoutes implements ApplicationRunner {
 
-    private static List<String> listOfStops;
-    private String filePath;
+  private static Map<Integer, Map<Integer, Integer>> routeMap;
+  private String filePath;
 
-    /**
-     * Here write logic for loading and storing the file in-memory
-     *
-     * @param args
-     * @throws Exception
-     */
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        List<String> value = args.getOptionValues("fileloc");
-        filePath = value.get(0);
-        listOfStops = createBusRouteFromFile();
-    }
+  /**
+   * Here write logic for loading and storing the file in-memory
+   *
+   * @param args
+   * @throws Exception
+   */
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    List<String> value = args.getOptionValues("fileloc");
+    filePath = value.get(0);
+    routeMap = createBusRouteFromFile();
+  }
 
 
-    public List<String> createBusRouteFromFile() {
-        List<String> list = new ArrayList<>();
-        try {
-            Scanner fileIn = new Scanner(new File(this.filePath));
-            int numberOfRoutes = Integer.parseInt(fileIn.nextLine());
-            while (fileIn.hasNext()) {
-                String inputLine = fileIn.nextLine().trim();
-                String routeFromLine = inputLine.substring(inputLine.indexOf(" ")).concat(" ");
-                list.add(routeFromLine);
-            }
-            if (list.size() == numberOfRoutes) {
-                return list;
-            } else {
-                return Collections.emptyList();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return Collections.emptyList();
+  public  Map<Integer, Map<Integer, Integer>> createBusRouteFromFile() {
+    try {
+      Scanner fileIn = new Scanner(new File(this.filePath));
+      int numberOfRoutes = Integer.parseInt(fileIn.nextLine());
+      Map<Integer, Map<Integer, Integer>> routeMap = new HashMap<>();
+      while (fileIn.hasNext()) {
+        String inputLine = fileIn.nextLine().trim();
+        String[] routeSplit = inputLine.split(" ");
+        int routeNumber = Integer.parseInt(routeSplit[0]);
+        Map<Integer, Integer> individualRouteMap = new HashMap<>();
+        for(int i = 0; i<routeSplit.length; i++){
+          individualRouteMap.put(Integer.parseInt(routeSplit[i]), i);
         }
-
+        routeMap.put(routeNumber, individualRouteMap);
+      }
+      if (routeMap.size() == numberOfRoutes) {
+        return routeMap;
+      } else {
+        return Collections.emptyMap();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      return Collections.emptyMap();
     }
 
-    public List<String> getListOfStops() {
-        return listOfStops;
-    }
+  }
+
+  public Map<Integer, Map<Integer, Integer>> getRouteMap() {
+    return routeMap;
+  }
 }
