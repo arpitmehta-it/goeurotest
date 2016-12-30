a39c0368fc1fea7a63c4fd6f633604e4753c2939
arpitmehta-it <arpitmehta_it@yahoo.com>
2016-12-30 13:25:55 +0100

    Updating service.sh

--- template/service.sh
+++ template/service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Keep the pwd in mind !!
+# Keep the pwd in mind!!
 # Example: RUN="java -jar $DIR/target/magic.jar"
 RUN=
 NAME=
